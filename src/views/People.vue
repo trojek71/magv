f2d9879315ae1396643f7a450f33cd4e4d958311
tomekr <tomekr@gmail.com>
2020-03-12 14:40:28 +0100

    Add mdb table

--- src/views/People.vue
+++ src/views/People.vue
@@ -13,80 +13,23 @@
     <input v-model="person.email" placeholder="Email"/>
     <button @click="handleCreatePerson">Create Person</button>
   
+  <mdb-tbl>
+    <mdb-tbl-head>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Email</th>
+      </tr>
+    </mdb-tbl-head>
+    <mdb-tbl-body>
+      <tr v-for="person in people" :key="person.id">
+      <td>{{person.id}}</td>
+      <td>{{person.name}}</td>
+      <td>{{person.email}}</td>
+      </tr>
+    </mdb-tbl-body>
+  </mdb-tbl>
   
-  <b-table-simple hover small caption-top responsive>
-    <caption>Items sold in August, grouped by Country and City:</caption>
-    <colgroup><col><col></colgroup>
-    <colgroup><col><col><col></colgroup>
-    <colgroup><col><col></colgroup>
-    <b-thead head-variant="dark">
-      <b-tr>
-        <b-th colspan="2">Region</b-th>
-        <b-th colspan="3">Clothes</b-th>
-        <b-th colspan="2">Accessories</b-th>
-      </b-tr>
-      <b-tr>
-        <b-th>Country</b-th>
-        <b-th>City</b-th>
-        <b-th>Trousers</b-th>
-        <b-th>Skirts</b-th>
-        <b-th>Dresses</b-th>
-        <b-th>Bracelets</b-th>
-        <b-th>Rings</b-th>
-      </b-tr>
-    </b-thead>
-    <b-tbody>
-      <b-tr>
-        <b-th rowspan="3">Belgium</b-th>
-        <b-th class="text-right">Antwerp</b-th>
-        <b-td>56</b-td>
-        <b-td>22</b-td>
-        <b-td>43</b-td>
-        <b-td variant="success">72</b-td>
-        <b-td>23</b-td>
-      </b-tr>
-      <b-tr>
-        <b-th class="text-right">Gent</b-th>
-        <b-td>46</b-td>
-        <b-td variant="warning">18</b-td>
-        <b-td>50</b-td>
-        <b-td>61</b-td>
-        <b-td variant="danger">15</b-td>
-      </b-tr>
-      <b-tr>
-        <b-th class="text-right">Brussels</b-th>
-        <b-td>51</b-td>
-        <b-td>27</b-td>
-        <b-td>38</b-td>
-        <b-td>69</b-td>
-        <b-td>28</b-td>
-      </b-tr>
-      <b-tr>
-        <b-th rowspan="2">The Netherlands</b-th>
-        <b-th class="text-right">Amsterdam</b-th>
-        <b-td variant="success">89</b-td>
-        <b-td>34</b-td>
-        <b-td>69</b-td>
-        <b-td>85</b-td>
-        <b-td>38</b-td>
-      </b-tr>
-      <b-tr>
-        <b-th class="text-right">Utrecht</b-th>
-        <b-td>80</b-td>
-        <b-td variant="danger">12</b-td>
-        <b-td>43</b-td>
-        <b-td>36</b-td>
-        <b-td variant="warning">19</b-td>
-      </b-tr>
-    </b-tbody>
-    <b-tfoot>
-      <b-tr>
-        <b-td colspan="7" variant="secondary" class="text-right">
-          Total Rows: <b>5</b>
-        </b-td>
-      </b-tr>
-    </b-tfoot>
-  </b-table-simple>
 </div>
 </template>
 
@@ -94,7 +37,7 @@
 import { PEOPLE } from '@/gql/queries/person.queries'
 import { PERSON_CREATED } from '@/gql/subscriptions/person.subscriptions'
 import { createPerson } from '@/gql/mutations/person.mutations'
-
+import { mdbTbl, mdbTblHead, mdbTblBody,mdbBtn } from 'mdbvue';
 export default {
   apollo: {
     people: {
@@ -112,7 +55,13 @@ export default {
       }
     }
   },
-  
+  components: {
+      mdbTbl,
+      mdbTblHead,
+      mdbTblBody,
+      mdbBtn
+    },
+
   methods: {
     handleCreatePerson() {
       this.$apollo.mutate({
@@ -136,7 +85,7 @@ export default {
 
 <style scoped>
   .container {
-    width: 80%;
+    width: 100%;
     min-height: 100vh;
     align-items: center;
     margin: 0 auto;
